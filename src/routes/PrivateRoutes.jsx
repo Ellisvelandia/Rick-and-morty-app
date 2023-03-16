159562a5eef12eaf38412f79aeb24f796776b505
Ellisvelandia <eyis619l@gmail.com>
2023-03-16 02:01:48 -0500

    routes privates

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -5,7 +5,17 @@ import { useSelector } from "react-redux";
 const PrivateRoutes = () => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      localStorage.setItem("isAuthenticated", "true");
+    } else {
+      localStorage.removeItem("isAuthenticated");
+    }
+  }, [isAuthenticated]);
+
+  const hasSession = localStorage.getItem("isAuthenticated");
+
+  return hasSession ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
