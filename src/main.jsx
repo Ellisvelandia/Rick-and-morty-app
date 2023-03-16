72bf0aa02acaae80aea70bdf9b46f9d34a4bc410
Ellisvelandia <eyis619l@gmail.com>
2023-03-16 13:25:25 -0500

    local storage with redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import RickAndMortyApp from "./RickAndMortyApp";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RickAndMortyApp />
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <RickAndMortyApp />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./useSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
